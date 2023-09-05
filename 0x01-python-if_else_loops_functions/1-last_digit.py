619a55fa0912a38aafa095c13c2ab6928d1b65bc
group12343 <onegroup935@gmail.com>
2023-09-05 13:47:54 -0700

    l

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+digit = abs(number) % 10
+if number < 0:
+    digit = -digit
+print("Last digit of {} is {} and is ".format(number, digit), end="")
+if digit > 5:
+    print("greater than 5")
+elif digit == 0:
+    print("0")
+else:
---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# 10-add.py
+
+def add(a, b):
+    """Return the addition of a and b."""
+    return (a + b)

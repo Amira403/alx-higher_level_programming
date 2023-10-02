1f8c10089673a2e080b5a43e4dcfc2a20e3e39e9
root <root@Mustafa>
2023-10-03 01:19:03 +0300

    hello, im in bin

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -79,7 +79,7 @@ class Rectangle:
         if self.__width == 0 or self.__height == 0:
             return ""
         else:
-            return ((str(self.print_symbol) * self.__width + "\n") * self.__height)[:-1]
+            return 2 * (self.__width + self.__height)
 
     def __repr__(self):
         """
